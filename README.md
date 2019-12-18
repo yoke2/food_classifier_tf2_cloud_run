e3fdf6da7779807f4ce3af99264ecd1c668d3188
yoke2 <wongyokekeong@gmail.com>
2019-12-19 00:12:29 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# food_classifier_tf2_cloud_run
-Food Classifier App using TensorFlow 2, Google Cloud Run
+# Food Classifier App with TensorFlow 2.0 model
+
+This repo contains a web app for classifying 3 types of local Singaporean Food using the following APIs and platforms
+
+- TensorFlow 2.0, using tf.image, tf.keras.models and tf.data (for training)
+- Starlette
+
+----------
+## Docker Installation
+
+You can test your changes locally by installing Docker and using the following command:
+
+```
+docker build -t food-tf2 . && docker run --rm -it -p 5000:5000 food-tf2
+```
+
+----------
+## Local Installation
+
+* Install dependencies
+```
+$ pip install -r requirements.txt
+```
+
+## Run app
+```
+$ python app/server.py serve
+```
+
+----------
+## Reference
+
+* Based on [Guide for production deployment to Render](https://course.fast.ai/deployment_render.html)
---
+++ app/server.py
@@ -0,0 +1,72 @@
+import aiohttp
+import asyncio
+import uvicorn
+import tensorflow as tf
+import sys
+from pathlib import Path
+from tensorflow.keras.models import load_model
+from ytb.tftools import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+export_file_url = 'https://drive.google.com/uc?export=download&id=1j_IIIlNGsgbZKrbnBf3M1qfd9BEJHb40'
+export_file_name = 'model2.h5'
+
+classes = ['mee_goreng', 'chicken_rice', 'roti_prata']
+valid_aug = [crop]
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+
+async def setup_learner():
+    await download_file(export_file_url, path/'models'/export_file_name)
+    try:
+        learn = load_model(str(path/'models'/export_file_name))
+        return learn
+    except RuntimeError as e:
+            print(e)
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    try:
+        img = process_img_bytes(img_bytes, img_size=224, augments=valid_aug)
+        label_arg = learn.predict(img, steps=1).argmax(axis=-1)[0]
+        prediction = classes[label_arg]
+    except Exception as e:
+        prediction = str(e)
+    return JSONResponse({'result': str(prediction)})
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
